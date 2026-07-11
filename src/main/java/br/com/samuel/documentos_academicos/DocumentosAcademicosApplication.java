adf964afb1ede76bd419e318d18dd1199427203d
SamuelDeMarco-Dev <samuel.demarco.dev@gmail.com>
2026-07-10 21:41:29 -0300

    feat: Inicializar a API com Java 21, Spring Boot e Maven.

---
+++ src/main/java/br/com/samuel/documentos_academicos/DocumentosAcademicosApplication.java
@@ -0,0 +1,13 @@
+package br.com.samuel.documentos_academicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentosAcademicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentosAcademicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/samuel/documentos_academicos/DocumentosAcademicosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.samuel.documentos_academicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentosAcademicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
