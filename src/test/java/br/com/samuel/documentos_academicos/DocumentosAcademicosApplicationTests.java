2de419a867747fe9e540748ac8898872d5e44364
SamuelDeMarco-Dev <samuel.demarco.dev@gmail.com>
2026-07-10 22:43:16 -0300

    feat: configura conexão com PostgreSQL e perfis de ambiente

--- src/test/java/br/com/samuel/documentos_academicos/DocumentosAcademicosApplicationTests.java
+++ src/test/java/br/com/samuel/documentos_academicos/DocumentosAcademicosApplicationTests.java
@@ -2,12 +2,13 @@ package br.com.samuel.documentos_academicos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class DocumentosAcademicosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
