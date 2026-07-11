349cfd0df614b686c5b26d2fdc99c64daa29ab93
SamuelDeMarco-Dev <samuel.demarco.dev@gmail.com>
2026-07-11 15:56:08 -0300

    feat: Implementa entidades e enums do dominio

---
+++ src/main/java/br/com/samuel/documentos_academicos/entity/TipoDocumento.java
@@ -0,0 +1,18 @@
+package br.com.samuel.documentos_academicos.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tipo_documento")
+@Getter @Setter
+public class TipoDocumento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false, length = 150, unique = true)
+    private String nome;
+}
